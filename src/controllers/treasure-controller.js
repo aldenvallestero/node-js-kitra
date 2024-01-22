db2f4b63ef6f5de088f31e3ee93293d825c92137
Alden Vallestero <alden.v@agtuary.com>
2024-01-22 19:27:17 +0800

    ♻️ Desctructure Response Usage

--- src/controllers/treasure-controller.js
+++ src/controllers/treasure-controller.js
@@ -1,13 +1,15 @@
-const TreasureService = require('../services/treasure-service');
+const TreasureService = require('../services/treasure-service')
 
 module.exports.findTreasure = async (req, res) => {
   const treasureService = new TreasureService();
-  const result = await treasureService.findTreasure(req.body);
-  return res.status(result.status).send(result.data);
+  const result = await treasureService.findTreasure(req.body)
+  res.status(result.status)
+  res.send(result.data)
 }
 
 module.exports.createTreasure = async (req, res) => {
-  const treasureService = new TreasureService();
-  const result = await treasureService.createTreasure(req.body);
-  return res.status(result.status).send(result.data);
+  const treasureService = new TreasureService()
+  const result = await treasureService.createTreasure(req.body)
+  res.status(result.status)
+  res.send(result.data)
 }
